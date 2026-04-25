5725b5cb810b56986c464604f3bd089a746418c6
Hans-Martin Will <hwill@acm.org>
2026-04-25 12:28:47 -0700

    Apply license header to files

--- kernel/src/program/expr.rs
+++ kernel/src/program/expr.rs
@@ -1,3 +1,17 @@
+// Copyright 2026 The Eigenius Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //! Parse Eigon-JSON expression resources into Mini-TT terms.
 //!
 //! Each expression form (Let, Apply, Var, Lambda, etc.) maps 1:1
